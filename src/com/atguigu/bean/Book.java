5a42615961a12228fd30fa6b07298b1165ba24fa
Kay <164293679@qq.com>
2020-02-11 20:21:35 +0800

    Initial commit

---
+++ src/com/atguigu/bean/Book.java
@@ -0,0 +1,8 @@
+package com.atguigu.bean;
+
+/**
+ * @author Kay
+ * @create 2020-02-11 14:25
+ */
+public class Book {
+}
---
+++ src/com/atguigu/bean/HelloTest.java
@@ -0,0 +1,17 @@
+package com.atguigu.bean;
+
+
+import java.util.ArrayList;
+
+public class HelloTest {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+    }
+
+    public void a(){
+
+    }
+    public void b(){
+
+    }
+}
